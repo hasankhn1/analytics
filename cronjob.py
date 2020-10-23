84ffe7787f371df1107f50a8065183b2b32c0d27
Hassan <h.karim@sunandsandsports.com>
2020-10-23 20:00:44 +0400

    ksa job

--- cronjob.py
+++ cronjob.py
@@ -5,8 +5,8 @@ from main import ksa_job,uae_job,kuwaitjob
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(ksa_job, "interval", seconds=3)
+scheduler.add_job(ksa_job, "interval", seconds=30)
 scheduler.add_job(uae_job, "interval", seconds=30)
-scheduler.add_job(kuwaitjob, "interval", seconds=30)
+scheduler.add_job(kuwaitjob, "interval", seconds=3)
 
 scheduler.start()
