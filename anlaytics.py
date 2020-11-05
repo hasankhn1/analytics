561c5077724a209759c7cb5d0cb4009a27c3715d
Hassan <h.karim@sunandsandsports.com>
2020-11-05 16:03:58 +0400

    change file name

--- anlaytics.py
+++ anlaytics.py
@@ -40,7 +40,7 @@ def upload_to_aws_sfcc(local_file, bucket, s3_file):
 
 
 uploaded = upload_to_aws(
-    'test_orders_uae.csv', 'sss-sfcc-ocapi-output', 'test_stitch/uae_orders.csv')
+    'test_orders_uae.csv', 'sss-sfcc-ocapi-output', 'test_stitch/uae_test_orders.csv')
 uploaded = upload_to_aws(
     'orders_uae.csv', 'sss-sfcc-ocapi-output', 'uae_orders.csv')
 uploaded = upload_to_aws(
